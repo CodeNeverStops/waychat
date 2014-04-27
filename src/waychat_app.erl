38b1e5e1fd17479278c420f926a8fc1eaf4a9e4e
Wayne.You <lokiwuxi@gmail.com>
2014-04-27 10:13:57 +0800

    add websocket support

--- src/waychat_app.erl
+++ src/waychat_app.erl
@@ -10,6 +10,15 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile([
+                                      {'_', [
+                                             {"/", cowboy_static, {priv_file, websocket, "index.html"}},
+                                             {"/websocket", ws_handler, []},
+                                             {"/static/[...]", cowboy_static, {priv_dir, websocket, "static"}}
+                                            ]}
+                                     ]),
+    {ok, _} = cowboy:start_http(http, 100, [{port, 8080}],
+                                [{env, [{dispatch, Dispatch}]}]),
     waychat_sup:start_link().
 
 stop(_State) ->
